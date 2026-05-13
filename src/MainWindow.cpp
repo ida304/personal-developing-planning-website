cb61a5f029bc056de0978bb9c7d75383e1c93493
Weizixin <202583211@uibe.edu.cn>
2026-05-13 14:48:47 +0800

    [Progress] 完成培养进度追踪模块核心逻辑与UI集成

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,12 +2,13 @@
 #include <QTabWidget>
 #include "profile/ProfileWidget.h"
 #include "course/CourseWidget.h"
-
+#include "progress/ProgressWidget.h"
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     QTabWidget *tabWidget = new QTabWidget(this);
     tabWidget->addTab(new ProfileWidget, "个人资料");
     tabWidget->addTab(new CourseWidget, "课程管理");
+    tabWidget->addTab(new ProgressWidget, "培养进度追踪");
     // 后续其他模块的界面可以继续添加
     setCentralWidget(tabWidget);
     resize(1000, 700);
