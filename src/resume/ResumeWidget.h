f2d399f1f4c42abce358ec768061dbaeb8d3e9ea
WeiZixin <202583211@uibe.edu.cn>
2026-05-19 14:43:13 +0800

    feat:简历生成

--- src/resume/ResumeWidget.h
+++ src/resume/ResumeWidget.h
@@ -2,32 +2,33 @@
 #define RESUMEWIDGET_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QComboBox>
 
+// 前向声明
 class DataManager;
-class QVBoxLayout;
-class QComboBox;
-class QTextEdit;
 
 class ResumeWidget : public QWidget
 {
     Q_OBJECT
-
 public:
-
-    explicit ResumeWidget(DataManager *dataManager, QWidget *parent = nullptr);
+    // 单参数构造函数
+    explicit ResumeWidget(QWidget *parent = nullptr);
     ~ResumeWidget() override;
 
-private:
-    // 初始化UI
-    void initUI();
-    // 模板切换函数
+    void setDataManager(DataManager *manager);
+
+private slots:
     void onTemplateChanged(int index);
 
 private:
-    DataManager *m_dataManager = nullptr;
+    void initUI();
+
     QVBoxLayout *m_mainLayout = nullptr;
     QComboBox *m_templateSelector = nullptr;
     QTextEdit *m_editor = nullptr;
+    DataManager *m_dataManager = nullptr;
 };
 
 #endif // RESUMEWIDGET_H
